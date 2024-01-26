422a1d3aa6b0d676163428bb40b5fdad90ad3fb1
Tony Ramírez <tonyramirez.business@outlook.com>
2024-01-25 23:17:07 -0600

    feat(part3/phonebook): add delete route
    exercise 3.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -50,4 +50,10 @@ app.get('/api/persons/:id', (req, res) => {
     res.send(person);
   }
   res.status(404).end();
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
 });
