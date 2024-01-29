82aa47afce4560436ba07854f93e691a4a562948
Tony Ramírez <tonyramirez.business@outlook.com>
2024-01-29 11:21:57 -0600

    feat(part3/phonebook): add update endpoint
    exercise 3.17

--- index.js
+++ index.js
@@ -28,29 +28,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.use(cors());
 app.use(errorHandler);
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/info', (_, res, next) => {
   Person.find({})
     .then((persons) => {
@@ -95,13 +72,6 @@ app.post('/api/persons', (req, res, next) => {
     });
   }
 
-  // ignore this check for now
-  // if (persons.find((person) => person.name === body.name)) {
-  //   return res.status(400).json({
-  //     error: 'name must be unique',
-  //   });
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -111,6 +81,33 @@ app.post('/api/persons', (req, res, next) => {
     .save()
     .then((savedPerson) => {
       res.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).json({
+      error: 'body missing',
+    });
+  }
+
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: `content missing: ${body.name ? 'number' : 'name'}`,
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
